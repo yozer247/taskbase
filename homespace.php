4442d61fd519e374407c2a70aa75a4629b3432f8
yozer247 <ndtintinger@gmail.com>
2020-07-10 20:32:54 +0100

    Create homespace.php

---
+++ homespace.php
@@ -0,0 +1,32 @@
+<? //this is php ?>
+<!DOCTYPE php>
+
+
+<!-- this is a HTML comment -->
+
+<!DOCTYPE HTML> <!-- setup the HTML -->
+<html>
+<head>
+  <meta charset="UTF-8"> <!-- can add more metadata here -->
+  <title>Homespace</title>
+</head>
+
+<body>
+  <h1><b>Welcome to your Homespace</b></h1>
+  <p> Hello Nathan, let's get started. </p>
+  <p> This site will help you keep calm, and carry out tasks. I hope you're staying hydrated. <br><br></p>
+  <p>You'll find your daily, weekly, and scheduled tasks below. Check the tick box to confirm completion and resume wholesomeness. <br> </p>
+  <p> Remember, the game is in your mind. Strive to be good to those around you. </p>
+
+
+  <h2>Tasks</h2>
+
+php time
+
+
+<br><br><br><br>
+
+<p> To add a task follow me to the following <a href="tasks.php">page~</a>
+
+</body>
+</html>
