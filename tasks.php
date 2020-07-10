9ce3b3853226694f722ecfd9725fcb6da46d56c6
yozer247 <ndtintinger@gmail.com>
2020-07-10 20:35:20 +0100

    Add files via upload

---
+++ tasks.php
@@ -0,0 +1,62 @@
+<? //this is a php comment ?>
+<!-- this is a html comment -->
+
+
+<?php
+
+$user = 'root';
+$password = 'root';
+$db = 'inventory';
+$host = 'localhost';
+$port = 8889;
+
+$link = mysqli_init();
+$success = mysqli_real_connect(
+   $link,
+   $host,
+   $user,
+   $password,
+   $db,
+   $port
+);
+
+?>
+
+
+
+
+
+
+<!DOCTYPE HTML> <!-- setup the HTML -->
+<html>
+<head>
+  <meta charset="UTF-8"> <!-- can add more metadata here -->
+  <title>Tasks Interaction</title>
+</head>
+
+<body>
+  <h1>Tasks Setup</h1>
+  <p>Here you can add, remove, and display tasks from the database.</p>
+
+<form method="post" action="">
+  <label for="title">Task Title:</label>
+  <input type="text" id="title" name="title"><br><br>
+  <label for="priority">Priority of Task:</label>
+  <select id="priority" name="priority">
+    <option value="High">High</option>
+    <option value="High">Medium</option>
+    <option value="High">Low</option>
+  </select>
+  <br><br>
+  <label for="description">Description:</label>
+  <input type="text" id="description" name="description"><br><br> <!-- make this bigger -->
+  <label for="deadline">Deadline:</label>
+  <input type="date" id="deadline" name="deadline"><br><br>
+  <input type="submit" value="Submit">
+</form>
+
+
+
+
+</body>
+</html>
